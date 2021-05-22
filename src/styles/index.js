cbf05eb4af0fced2ed42534243172cbe4b071b56
Kim Frederiksen <kim@heyrobot.com>
2021-05-22 20:03:50 +0200

    Delete src directory

--- src/styles/index.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from "@material-ui/core/styles";
-
-export const useStyles = makeStyles(() => ({
-  formControl: {
-    // margin: ".5rem",
-    // minWidth: "12rem",
-    // width: "100%",
-  },
-}));
